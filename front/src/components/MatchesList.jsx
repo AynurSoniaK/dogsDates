136381206689f096ab7ab2854e012db98724c638
Aynur_Sonia <aynur@tkorp.com>
2023-08-26 23:11:20 +0200

    ajout loading opacity

--- front/src/components/MatchesList.jsx
+++ front/src/components/MatchesList.jsx
@@ -10,8 +10,7 @@ const MatchesList = ({ matches, setMatchClicked, onChildMatchListChange }) => {
   const matchedUserIds = matches.length > 0 ? matches.map(({ user_id }) => user_id) : [];
   const [matchListReady, setMatchListReady] = useState(false)
   const [bothLiked, setBothLiked] = useState([])
-  const [messagesSeen, setMessagesSeen] = useState([])
-  const [loading, setLoading] = useState(true);
+  const [messagesSeenReady, setMessagesSeenReady] = useState(false)
 
   const userId = cookies.UserId
   let navigate = useNavigate()
@@ -60,8 +59,8 @@ const MatchesList = ({ matches, setMatchClicked, onChildMatchListChange }) => {
           `${process.env.REACT_APP_API_URL}/messages`,
           {
             params: {
-              toUserId: dog.user_id,
-              fromUserId: userId,
+              toUserId: userId,
+              fromUserId: dog.user_id
             }
           }
         );
@@ -75,7 +74,7 @@ const MatchesList = ({ matches, setMatchClicked, onChildMatchListChange }) => {
       }));
 
       setDogMatched(updatedDogProfiles);
-      setLoading(false)
+      setMessagesSeenReady(true)
     } catch (err) {
       navigate('/error');
     }
@@ -129,8 +128,8 @@ const MatchesList = ({ matches, setMatchClicked, onChildMatchListChange }) => {
                 {el.messages.filter(message => !message.read).length}
               </span>
             )}
-            <div className="imgContainer" onClick={() => updateMsgSeen(el)}>
-              <img src={el.url} alt="profile-pics" />
+            <div className="imgContainer">
+              { messagesSeenReady ? <img src={el.url} alt="profile-pics" onClick={() => updateMsgSeen(el)}/> : <img src={el.url} className="opacityImg" alt="profile-pics"/>}
             </div>
             <h3>{el.name}
               <svg className='trash' onClick={() => deleteUserMatch(el.user_id)} xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 448 512"><path d="M170.5 51.6L151.5 80h145l-19-28.4c-1.5-2.2-4-3.6-6.7-3.6H177.1c-2.7 0-5.2 1.3-6.7 3.6zm147-26.6L354.2 80H368h48 8c13.3 0 24 10.7 24 24s-10.7 24-24 24h-8V432c0 44.2-35.8 80-80 80H112c-44.2 0-80-35.8-80-80V128H24c-13.3 0-24-10.7-24-24S10.7 80 24 80h8H80 93.8l36.7-55.1C140.9 9.4 158.4 0 177.1 0h93.7c18.7 0 36.2 9.4 46.6 24.9zM80 128V432c0 17.7 14.3 32 32 32H336c17.7 0 32-14.3 32-32V128H80zm80 64V400c0 8.8-7.2 16-16 16s-16-7.2-16-16V192c0-8.8 7.2-16 16-16s16 7.2 16 16zm80 0V400c0 8.8-7.2 16-16 16s-16-7.2-16-16V192c0-8.8 7.2-16 16-16s16 7.2 16 16zm80 0V400c0 8.8-7.2 16-16 16s-16-7.2-16-16V192c0-8.8 7.2-16 16-16s16 7.2 16 16z" /></svg>
